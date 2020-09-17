ff933f4513ad3e51cb46fbe67a3e5b7c6e279b6a
donald <onsemise@qq.com>
2020-09-17 17:26:08 +0800

    getDb

--- app/src/main/java/com/dhy/logswitch/demo/room/RequestDao.kt
+++ app/src/main/java/com/dhy/logswitch/demo/room/RequestDao.kt
@@ -1,7 +1,10 @@
 package com.dhy.logswitch.demo.room
 
+import android.content.Context
 import androidx.paging.DataSource
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Dao
 interface RequestDao {
@@ -15,4 +18,19 @@ interface RequestDao {
 @Database(entities = [RequestLog::class], version = 1, exportSchema = false)
 abstract class NetLogDatabase : RoomDatabase() {
     abstract fun requestDao(): RequestDao
+}
+
+private var dao: RequestDao? = null
+fun getDb(context: Context? = null): RequestDao? {
+    if (dao != null || context == null) return dao
+    val migration: Migration = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+        }
+    }
+    dao = Room.databaseBuilder(context.applicationContext, NetLogDatabase::class.java, "NetLog.db")
+        .allowMainThreadQueries()
+        .addMigrations(migration)
+        .build()
+        .requestDao()
+    return dao
 }
